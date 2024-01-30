eeb865f60c4c1f4dfe13589ace37523dc5b84b72
technqvi <technqvi_gangster@hotmail.com>
2024-01-30 10:57:43 +0700

    ok

---
+++ IncidentSummarization_API/JobRun_Summarization.bat
@@ -0,0 +1,5 @@
+call ../.venv/Scripts/activate.bat
+python.exe job_run.py
+pause
+
+
--- IncidentSummarization_API/job_run.py
+++ IncidentSummarization_API/job_run.py
@@ -13,15 +13,18 @@
 # else:
 #     print(f"Error: {response.status_code}")
 
+# write code python read line to get input console
 
 
-def get_incident_summarization_by_id():
+
+def get_incident_summarization_by_id(incident_id,model_id):
     import incident_content_creator as cc
     import text_bison as bison
     import text_gemini as gemini
     from dotenv import dotenv_values
     import os
-    incident_id= 4300 #4431  #4300  #4438 # 3743 long sample AIS
+    
+    inout_path="test-input-output"
     config = dotenv_values('.env')
     content_template="v2_incident_template.txt"
     content_text=cc.get_incident_content(incident_id=incident_id,content_template=content_template,config=config)
@@ -31,26 +34,36 @@ def get_incident_summarization_by_id():
     print(f"=============Total length of content_text {n_str}====================")
     print(n_str)
 
- 
-    file_path = "incident_conent.txt"
-    with open(file_path, 'w') as file:
-        file.write(content_text)
-
-    # incident_summarization=bison.summarize_incident(content_text,config)
+    if model_id==1: 
+        incident_summarization=bison.summarize_incident(content_text,config)
         
-    incident_summarization=""
-    responses=gemini.summarize_incident(content_text,config)
-    for response in responses:
-        # incident_summarization=incident_summarization+response       
-        print(response.text, end="")
+    else:    
+        incident_summarization=""
+        responses=gemini.summarize_incident(content_text,config)
+        for response in responses:
+            # incident_summarization=incident_summarization+response       
+            print(response.text, end="")
 
+    print(f"=============Total length of summary_text {len(incident_summarization)}====================")
 
-    output_file_path = "incident_summary.txt"
-    with open(output_file_path, 'w') as file:
+    file_path = f"{incident_id}-conent_model{model_id}.txt"
+    with open(os.path.join(inout_path,file_path), 'w') as file:
+        file.write(content_text)
+    output_file_path = f"{incident_id}-_summary_model{model_id}.txt"
+    with open(os.path.join(inout_path,output_file_path), 'w') as file:
         # Write the content to the file
         file.write(incident_summarization)
 
 if __name__ == "__main__":
-    get_incident_summarization_by_id()
+    incident_id= 4300 #4431  #4300  #4438 # 3743 long sample AIS
+    model_id = 1
+
+    print("Enter the following incident id: ")
+    id = input("Incident ID : ")
+
+    # print("1=bison and 2=gemini ")
+    # model_id = input("Model ID : ")
+
+    get_incident_summarization_by_id(int(id),int(model_id))
 
 
